e0301732ddb15603e7b72ab5ec24c98562de84c6
maxvwalbert <mvwa@comcast.net>
2016-04-08 01:34:29 -0400

    Notifications work now when you press a button
    holla

--- popup.js
+++ popup.js
@@ -2,30 +2,37 @@ var options1 = {
   type: "basic",
   title: "Homework Mode Activated!",
   iconUrl: "Icon.png",
-  body: "We'll let you know if you get distracted"
+  message: "We'll let you know if you get distracted"
 }
 
 var options2 = {
   type: "basic",
   title: "Distraction Alert!",
   iconUrl: "Icon.png",
-  body: "You still have unfinished homework :("
+  message: "You still have unfinished homework :("
 }
 
 var options3 = {
   type: "basic",
   title: "Homework Mode Deactivated",
   iconUrl: "Icon.png",
-  body: "No more homework means no more notifications. See you next time! :)"
+  message: "No more homework means no more notifications. See you next time! :)"
 }
+
+var penis = 1;
 
 function onStart() {
   console.log("buttsex");
   chrome.notifications.create(options1);
+  penis = 1337;
+}
+
+function onFinish() {
+  console.log(penis);
+  chrome.notifications.create(options3);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('activate').addEventListener('click', function() {
-    onStart();
-  });
+    document.getElementById('activate').addEventListener('click', onStart);
+    document.getElementById('deactivate').addEventListener('click', onFinish);
 });
